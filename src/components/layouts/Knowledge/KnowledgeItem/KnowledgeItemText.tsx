272028a3421d76ae4422cc76ea10e1ad139d9d64
StAl15 <stihinaleksander@yandex.ru>
2023-02-19 12:37:23 +0500

    add some sanity

--- src/components/layouts/Knowledge/KnowledgeItem/KnowledgeItemText.tsx
+++
@@ -1,9 +0,0 @@
-export const KnowledgeItemText: React.FC<{ text?: string, editable?: boolean }> = ({text, editable}) => {
-    return (
-        <>
-            <textarea
-                className={`rounded-2xl resize-${editable ? '' : 'none'} p-5 bg-white outline-0 font-montseratRegular h-auto min-h-96 w-[600px]`}
-                defaultValue={text ? text : ''} disabled={!editable} placeholder={'Введите текст'}/>
-        </>
-    );
-};
